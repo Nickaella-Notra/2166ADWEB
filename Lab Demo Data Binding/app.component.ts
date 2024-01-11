172ee9848c0764b478f3782cb08df9d6ffd4ac01
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2024-01-11 16:43:08 +0800

    app.component.ts

---
+++ Lab Demo Data Binding/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular Data Binding';
+  clientName = 'Nickaella Notra'; //property
+  appliedCSS = 'green'; //style
+  notAppliedCSS = false;
+  myColor = 'red';
+  center = 'center';
+
+  clickCount = 0 
+  clickMe(){
+    this.clickCount++;
+  }
+
+  resetClickCount(){
+    this.clickCount = 0;
+  }
+}
